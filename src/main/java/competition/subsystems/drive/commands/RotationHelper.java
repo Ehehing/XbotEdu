b2a6b66d6938904b0b6f87589d02273c0970be6a
Ehehing <kuroyukihimeowo@gmail.com>
2023-08-26 14:35:42 -0700

    test for rotation configuration

--- src/main/java/competition/subsystems/drive/commands/RotationHelper.java
+++ src/main/java/competition/subsystems/drive/commands/RotationHelper.java
@@ -3,6 +3,15 @@ package competition.subsystems.drive.commands;
 public class RotationHelper {
 
     public static double findSmallestDifferenceBetweenTwoAngles(double current, double goal) {
-        return 0;
+//  current 45 target 90 expect 45
+        //current 150 goal -120 expect 90
+        //current 179 goal -179 expect 2
+        //current -179 goal 179 expect -2
+        //-120 - 150 270 270 -360 = -90 when expect 90
+
+        
+
+
+
     }
 }
